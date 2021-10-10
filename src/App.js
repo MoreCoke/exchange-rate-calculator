4f912c999e7fc3dc73431a1ba0d28445e107b5d9
William Chen <seed45699@gmail.com>
2021-10-10 15:47:34 +0800

    Extract Keyboard from App

--- src/App.js
+++ src/App.js
@@ -1,93 +1,10 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import numeral from 'numeral';
+import React, { useMemo } from 'react';
 
 import Keyboard from './components/Keyboard';
+import useKeyboard from './hooks/useKeyboard';
 
 function App() {
-  const [currentNum, setCurrentNum] = useState('');
-  const [accumulation, setAccumulation] = useState('');
-  const [operator, setOperator] = useState('');
-
-  useEffect(() => {
-    console.log('currentNum: ', currentNum);
-    console.log('accumulation: ', accumulation);
-    console.log('operator: ', operator);
-  }, [currentNum, accumulation, operator]);
-
-  const onOperatorPress = (currentOperator) => {
-    switch (currentOperator) {
-      case '-':
-      case 'x':
-      case '+':
-      case '÷':
-        if (accumulation && currentNum && operator) {
-          calculate();
-          setOperator(currentOperator);
-          return;
-        }
-        if (currentNum) {
-          setOperator(currentOperator);
-          !accumulation && setAccumulation(currentNum);
-          setCurrentNum('');
-          return;
-        }
-        setOperator(currentOperator);
-        break;
-      case '=':
-        if (accumulation && currentNum && operator) {
-          calculate();
-          return;
-        }
-        break;
-      default:
-        break;
-    }
-  };
-
-  const onOtherPress = (other) => {
-    switch (other) {
-      case 'AC':
-        setCurrentNum('');
-        setAccumulation('');
-        setOperator('');
-        break;
-      case 'Swap':
-        //TODO
-        break;
-      case 'Currency':
-        //TODO
-        break;
-      default:
-        break;
-    }
-  };
-
-  const onNumPress = (num) => {
-    setCurrentNum((prev) => prev + num);
-  };
-
-  const calculate = () => {
-    let total = '0';
-    switch (operator) {
-      case '-':
-        total = numeral(accumulation).subtract(currentNum)._value + '';
-        break;
-      case 'x':
-        total = numeral(accumulation).multiply(currentNum)._value + '';
-        break;
-      case '+':
-        total = numeral(accumulation).add(currentNum)._value + '';
-        break;
-      case '÷':
-        total = numeral(accumulation).divide(currentNum)._value + '';
-        break;
-      default:
-        break;
-    }
-    setCurrentNum('');
-    setAccumulation(total);
-    setOperator('');
-  };
+  const { currentNum, accumulation, onOperatorPress, onNumPress, onOtherPress } = useKeyboard();
 
   const inputValue = useMemo(() => currentNum || accumulation, [currentNum, accumulation]);
 
